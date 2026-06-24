554d9a1bb376a6246cc165a55301ee15398ff52b
Adrian <afgomezro@unitru.edu.pe.com>
2026-06-23 23:47:33 -0500

    Simplificar regex del middleware para evitar crasheo en Vercel

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -31,6 +31,11 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/dashboard/:path*",
+    "/admin/:path*",
+    "/docente/:path*",
+    "/secretaria/:path*",
+    "/login",
+    "/"
   ],
 };
